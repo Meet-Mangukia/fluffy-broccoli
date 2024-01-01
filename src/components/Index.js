e4b19350f09ccb1b0186636b142a801633d3efa6
Mangukiajeel <jeelmangukia@gmail.com>
2024-01-01 14:04:29 +0530

    second

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,13 +1,21 @@
-import React,{useEffect} from 'react'
-// import './main.js';
+import React,{useEffect,useState} from 'react'
+import './main.js';
 import '../css/bootstrap.min.css'
 import '../css/style.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+
+import {Nav ,NavDropdown,Navbar} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 //import '../scss/bootstrap.scss';
 import user from '../img/user.jpg';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid'
 import CanvasJSReact from '@canvasjs/react-charts';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -177,6 +185,27 @@ function Index() {
     // Your code that depends on main.js can go here
   }, []);
 
+
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  // Function to toggle the dropdown state
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  useEffect(() => {
+    // Update the margin-top of the container based on the dropdown state
+    const container = document.querySelector('.move-downwards');
+    if (container) {
+      container.style.marginTop = isDropdownOpen ? '200px' : '0';
+    }
+  }, [isDropdownOpen]);
+
+
+
+ 
+
   return (
     <div class="container-xxl position-relative bg-white d-flex p-0">
         {/* <!-- Spinner Start --> */}
@@ -206,27 +235,48 @@ function Index() {
                 </div>
                 <div class="navbar-nav w-100">
                     <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-                    <div className="nav-item dropdown">
-                        <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-laptop me-2"></i>Elements</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="button.html" class="dropdown-item">Buttons</a>
-                            <a href="typography.html" class="dropdown-item">Typography</a>
-                            <a href="element.html" class="dropdown-item">Other Elements</a>
-                        </div>
-                    </div>
+                    
+
+                    {/* Dropdown starts */}
+                    <div id='mydiv'>
+                     <Dropdown className='' show={isDropdownOpen} onToggle={toggleDropdown}>
+                        <div className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+      <Dropdown.Toggle style={{color: 'black'}} className='btn-default' id='mydiv'>
+      <i class="fa fa-laptop me-2"></i>Elements
+      </Dropdown.Toggle></div>
+
+      <Dropdown.Menu className="dropdown-menu bg-transparent border-0">
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown></div>
+                    {/* Dropdown ends */}
                     <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
                     <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
                     <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
                     <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Pages</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="signin.html" class="dropdown-item">Sign In</a>
-                            <a href="signup.html" class="dropdown-item">Sign Up</a>
-                            <a href="404.html" class="dropdown-item">404 Error</a>
-                            <a href="blank.html" class="dropdown-item">Blank Page</a>
-                        </div>
-                    </div>
+                    
+
+
+                    {/* dropdown */}
+                   <div id='mydiv'>
+                     <Dropdown className=''>
+                        <div className="nav-link dropdown-toggle">
+      <Dropdown.Toggle style={{color: 'black'}} className='btn-default' id='mydiv'>
+      <i class="far fa-file-alt me-2" ></i>Pages
+      </Dropdown.Toggle></div>
+
+      <Dropdown.Menu className="dropdown-menu bg-transparent border-0">
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown></div>
+  
+                    {/* dropdown ends */}
+
+                    
                 </div>
             </nav>
         </div>
